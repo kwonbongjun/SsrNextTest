73c57db11b6a37ccbd9569931b77252ff1c82882
kwonbongjun <bongjun777@naver.com>
2021-05-31 20:52:01 +0900

    ssr started

---
+++ SsrNextWas/src/main/java/com/example/SpringSsrNextApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSsrNextApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSsrNextApplication.class, args);
+    }
+}
